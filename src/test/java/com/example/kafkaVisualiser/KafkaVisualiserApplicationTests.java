e6e088fa622726e242721450fd603b486213a20a
Deevesh Beegun <35701065+DeeveshBeegun@users.noreply.github.com>
2025-10-23 21:19:11 +0400

    Inital Commit

---
+++ src/test/java/com/example/kafkaVisualiser/KafkaVisualiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaVisualiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaVisualiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
